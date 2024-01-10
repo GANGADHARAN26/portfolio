e7f05c14ce0350585e61170caa08bd19fd17bebe
GANGADHARAN26 <gangadharana01@gmail.com>
2024-01-10 11:44:00 +0530

    added cv link and resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
           </li>
           <li className="py-2">
             <a
-              href="#"
+              href="https://docs.google.com/document/d/1Jr_nFm9OEfyJlQ7wGCIvzQGCXUU9sJ6C4Q3_dscpG40/edit?usp=sharing"
               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >
               Resume
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,7 @@
 import { FiDownload } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
 const About = () => {
+  const navigate=useNavigate()
   return (
     <div className="container mt-28">
       <div className="py-20 px-8 h-96 border-2 w-full backdrop-blur-3xl ">
@@ -9,19 +11,18 @@ const About = () => {
         <div className="body">
           <p className="text-white flex py-5">
             Hello everyone, I am Gangadharan from India and I have finished
-            Bachelors of Technology in Informational Technology. I have got
-            through three years of Diploma in Computer Engineering. I share
+            Bachelors of Technology in Informational Technology. I share
             interest in developing  Web Development Projects and also I have
             in doing IEEE Projects ant I have done two IEEE projects like Automated
             vehicle and  Alcohal control Technology in vehicle as my Bachelors
             Projects and I am currently working on learning Web Development Technology
             and Doing Projects 
           </p>
-          <button className="bg-white text p-3 border-yellow-500 border-4 flex gap-4 justify-center items-center">
-            <FiDownload className="" /> Download CV
-          </button>
+          <a href="https://drive.google.com/file/d/1dTIdUwaBHwKHHqGl9A4RI7S3qkS1Nb6C/view?usp=sharing" className="bg-white text p-3 border-yellow-500 border-4 flex gap-2 w-40 justify-center items-center">
+            <FiDownload className="block" /> Download CV
+          </a>
         </div>
-      </div>
+      </div >
     </div>
   );
 };
