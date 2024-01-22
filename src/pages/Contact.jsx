d6b55ce78eabd472f8af984e1d94e8ed5f3127ad
GANGADHARAN26 <gangadharana01@gmail.com>
2024-01-22 11:52:52 +0530

    added some layout changes in contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
   };
   return (
     <section className=" backdrop-blur-3xl">
-      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+      <div className="py-5 lg:py-15 px-4 mx-auto max-w-screen-md">
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
           Contact Us
         </h2>
